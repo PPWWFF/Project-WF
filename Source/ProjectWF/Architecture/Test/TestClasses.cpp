3efd23d3d4853f6af1efa50217250cbf1d17267b
Jaeguins <jsu4838@naver.com>
2023-12-21 23:51:05 +0900

    정상우 feat(Architecture) 아키텍쳐 테스트 클래스 작성

---
+++ Source/ProjectWF/Architecture/Test/TestClasses.cpp
@@ -0,0 +1,33 @@
+﻿#include "TestClasses.h"
+
+#include "Architecture/WFMvcHolder.h"
+
+void FWFTestModel::Tick(float DeltaTime)
+{
+	FWFModelBase::Tick(DeltaTime);
+}
+
+void AWFTestGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	
+}
+
+void AWFTestGameMode::AddAgent()
+{
+	TSharedPtr<FWFTestModel> testModel = FWFMvcHolder::Get().GetTestModel();
+ 	if(!testModel.IsValid())
+ 		return;
+ 
+ 	TSharedPtr<FWFTestView> testView = FWFMvcHolder::Get().GetTestView();
+ 	if(!testView.IsValid())
+ 		return;
+ 	
+ 	TSharedPtr<FWFTestModelAgent> testModelAgent = MakeShareable(new FWFTestModelAgent(testModel->GetUniqueId()));
+ 	if(!testModelAgent.IsValid())
+ 		return;
+ 
+ 	testModel->AddModelAgent(testModelAgent);
+ 	testView->AddViewAgent(testModelAgent->GetViewAgent());
+}
---
+++ Source/ProjectWF/Architecture/Test/TestClasses.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include "Architecture/Model/WFModelAgentBase.h"
+#include "Architecture/Model/WFModelBase.h"
+#include "Architecture/View/WFViewAgentBase.h"
+#include "Architecture/View/WFViewBase.h"
+#include "GameModule/WFGameModeBase.h"
+
+#include "TestClasses.generated.h"
+
+class FWFTestModel : public FWFModelBase
+{
+public:
+	virtual void Tick(float DeltaTime) override;
+};
+
+class FWFTestView : public FWFViewBase
+{
+public:
+	virtual void Tick(float DeltaTime) override;
+};
+
+class FWFTestModelAgent : public FWFModelAgentBase
+{
+public:
+	FWFTestModelAgent(uint64 id)
+		: FWFModelAgentBase(id)
+	{
+	}
+};
+
+class FWFTestViewAgent : public FWFViewAgentBase
+{
+public:
+	FWFTestViewAgent(uint64 id)
+		: FWFViewAgentBase(id)
+	{
+	}
+};
+
+UCLASS(Blueprintable)
+class AWFTestGameMode : public AWFGameModeBase
+{
+	GENERATED_BODY()
+public:
+	virtual void StartPlay() override;
+
+	UFUNCTION(BlueprintCallable,CallInEditor)
+	void AddAgent();
+};
+
+UCLASS(Blueprintable)
+class UWFTestAgentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	UWFTestAgentComponent();
+};
+
+UCLASS(Blueprintable)
+class AWFTestActor : public AActor
+{
+	GENERATED_BODY()
+public:
+	AWFTestActor();
+};
